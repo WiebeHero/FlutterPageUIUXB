f4360adaccf9cbbe8d978fcb27d5b63db6ef3f1c
WiebeHero <wiebesportel2001@hotmail.com>
2023-01-11 11:43:39 +0100

    Patch booger

--- lib/ContentManager/shop.dart
+++ lib/ContentManager/shop.dart
@@ -17,60 +17,79 @@ class Shop extends StatefulWidget{
 
 }
 
-class _Shop extends State<Shop>{
+class _Shop extends State<Shop> with TickerProviderStateMixin{
 
   double offsetX = 0;
   double offsetY = 0;
-  double angle = 3;
+  double angle = 2;
+  late AnimationController controller;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = AnimationController(duration: const Duration(seconds: 1), vsync: this);
+  }
+
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Transform.translate(
-      offset: const Offset(0, -100),
+      offset: const Offset(0, 0),
       child: SizedBox(
         width: 500,
-        height: 500,
+        height: 800,
         child: Container(
           color: Colors.black,
           alignment: Alignment.topCenter,
-          child: Stack(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              for(int i = 0; i < 4; i++) AnimatedContainer(
-                duration: const Duration(seconds: 1),
-                child: Transform.rotate(
-                  angle: angle * i / 180 * pi,
-                  origin: const Offset(-50, 75),
-                  child: Transform.translate(
-                    offset: Offset(0, 0),
-                    child: SizedBox(
-                      width: 100,
-                      height: 150,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.green,
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(color: Colors.black, width: 2, style: BorderStyle.solid),
+              Container(
+                padding: const EdgeInsets.only(top: 100),
+                width: 300,
+                height: 600,
+                alignment: Alignment.topCenter,
+                child: Stack(
+                  children: [
+                    for(int i = 0; i < 4; i++) RotationTransition(
+                      turns: Tween(begin: angle * i / 180 * pi, end: 0.0).animate(controller),
+                      child: SlideTransition(
+                        position: Tween<Offset>(begin: Offset.zero, end: Offset(-2.0 + i * 1.33, 1.5)).animate(controller),
+                        child: SizedBox(
+                          width: 100,
+                          height: 150,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.green,
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(color: Colors.black, width: 2, style: BorderStyle.solid),
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
               ),
-              Transform.translate(
-                offset: const Offset(0, 200),
-                child: GestureDetector(
-                  onTap: (){
-                    setState(() {
-                      print("Testing");
-                      angle = 0;
+              GestureDetector(
+                onTap: (){
+                  setState(() {
+                    print("Testing");
+                    controller.forward().whenComplete(() => {
+                      controller.reverse(),
                     });
-                  },
-                  child: SizedBox(
-                    width: 200,
-                    height: 300,
-                    child: Container(
-                      color: Colors.blue,
-                    ),
+                  });
+                },
+                child: SizedBox(
+                  width: 200,
+                  height: 100,
+                  child: Container(
+                    color: Colors.blue,
                   ),
                 ),
               )
